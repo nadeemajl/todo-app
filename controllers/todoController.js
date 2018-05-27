8df2e82392c240eadf39442ee0251645b0503a51
Nadeem Lalani <nadeemajl@gmail.com>
2018-05-27 09:41:25 +0530

    commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,15 @@
 module.exports = function(app){
 
-    app.get('/todo',function(req,res){
+app.get('/todo',function(req,res){
+    res.render('todo');
+});
 
-    });
+app.post('/todo',function(req,res){
+    
+});
 
-    app.post('/todo',function(req,res){
-        
-    });
-
-    app.delete('/todo',function(req,res){
-        
-    });
+app.delete('/todo',function(req,res){
+    
+});
     
 };
