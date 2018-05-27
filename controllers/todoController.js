59f3bcf54bc53b8b1199cee3053b3c6a5d8236ab
Nadeem Lalani <nadeemajl@gmail.com>
2018-05-27 10:09:05 +0530

    commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,17 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get milk'}, {item:'feed baby'},{item: 'clean room'},{item:'wash car'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
 app.get('/todo',function(req,res){
-    res.render('todo');
+    res.render('todo',{todos:data});
 });
 
-app.post('/todo',function(req,res){
-    
+app.post('/todo',urlencodedParser,function(req,res){
+    data.push(req.body);
+    res.json(data);
 });
 
 app.delete('/todo',function(req,res){
